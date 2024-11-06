a5780e0ac45e153a5527e20ac75ccc22a95330fd
Jules Portier <juportie@z2r9p1.42lyon.fr>
2024-11-06 12:07:38 +0100

    FEAT: ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,26 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juportie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 16:08:24 by juportie          #+#    #+#             */
-/*   Updated: 2024/11/05 17:13:50 by juportie         ###   ########.fr       */
+/*   Created: 2024/11/05 16:53:18 by juportie          #+#    #+#             */
+/*   Updated: 2024/11/06 12:05:16 by juportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t	i;
+	char	*byte;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *c);
-void	*ft_memset(void *s, int c, size_t n);
-
-#endif
+	i = 0;
+	byte = s;
+	while (i < n)
+	{
+		byte[i] = c;
+		i++;
+	}
+	return (s);
+}
