0703e81586b8f4750ca2643b72a2d87418315d69
Jules Portier <juportie@z2r9p1.42lyon.fr>
2024-11-12 09:21:04 +0100

    FIX(calloc): let malloc handle errors internally
    If size is zero malloc return NULL of a 0 size pointer that can be free.
    If size is negative malloc return NULL.

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: juportie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 11:02:18 by juportie          #+#    #+#             */
-/*   Updated: 2024/11/11 12:42:13 by juportie         ###   ########.fr       */
+/*   Updated: 2024/11/12 09:20:18 by juportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*mem;
 
 	if (
-		nmemb * size <= 0
-		|| nmemb > 2147483647 / size
+		size != 0
+		&& nmemb > 2147483647 / size
 	)
-	{
-		mem = malloc(1);
-		ft_bzero(mem, 1);
-		return (mem);
-	}
+		return (NULL);
 	mem = malloc(nmemb * size);
 	if (mem == NULL)
 		return (NULL);
