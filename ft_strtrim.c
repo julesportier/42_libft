626c08a1a699cacdbcb62e6c6920e68170b2ed59
Jules Portier <juportie@z2r9p1.42lyon.fr>
2024-11-14 16:29:33 +0100

    FIX(norm): line too long in strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: juportie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 13:23:00 by juportie          #+#    #+#             */
-/*   Updated: 2024/11/14 14:28:16 by juportie         ###   ########.fr       */
+/*   Updated: 2024/11/14 16:29:17 by juportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static int	char_in_set(char const c, char const *set)
 	return (0);
 }
 
-static size_t	get_trimmed_size(size_t start, size_t end)
+static size_t	get_trim_size(size_t start, size_t end)
 {
 	if (end <= start)
 		return (0);
@@ -37,23 +37,23 @@ static size_t	get_trimmed_size(size_t start, size_t end)
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start_pos;
-	size_t	end_limit;
+	size_t	end;
 	size_t	i;
 	char	*s_trimmed;
 
-	end_limit = ft_strlen(s1);
-	if (set[0] && end_limit)
+	end = ft_strlen(s1);
+	if (set[0] && end)
 	{
 		start_pos = 0;
 		while (char_in_set(s1[start_pos], set))
 			start_pos++;
-		while (char_in_set(s1[end_limit - 1], set))
-			end_limit--;
-		s_trimmed = malloc((get_trimmed_size(start_pos, end_limit) + 1) * sizeof(char));
+		while (char_in_set(s1[end - 1], set))
+			end--;
+		s_trimmed = malloc((get_trim_size(start_pos, end) + 1) * sizeof(char));
 		if (s_trimmed == NULL)
 			return (NULL);
 		i = 0;
-		while (start_pos + i++ < end_limit)
+		while (start_pos + i++ < end)
 			s_trimmed[i - 1] = s1[start_pos + i - 1];
 		s_trimmed[i - 1] = '\0';
 		return (s_trimmed);
