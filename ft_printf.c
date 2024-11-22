0200ba5aa5e9d9ec2f86cf55b8489bb3cb6718db
Jules Portier <juportie@z2r9p1.42lyon.fr>
2024-11-22 17:06:11 +0100

    FEAT: arg parsing

--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,12 @@
 /*   By: juportie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 11:27:10 by juportie          #+#    #+#             */
-/*   Updated: 2024/11/21 11:29:25 by juportie         ###   ########.fr       */
+/*   Updated: 2024/11/22 15:52:42 by juportie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <unistd.h>
-#include "libft/libft.h"
-
-#include <stdio.h>
+//#include "libft/libft.h"
+#include "printf.h"
 
 int	check_formaters(char c)
 {
@@ -58,44 +55,60 @@ int	count_formats(const char *str)
 	return (count);
 }
 
-//int	parse_type(const char format)
+int	parse_type(const char format, va_list ap)
+{
+	int	i;
+
+	i = 0;
+	if (format == 'c')
+		i = ft_putchar_fd(va_arg(ap, int), 1);
+	else if (format == 's')
+		i = ft_putstr_fd(va_arg(ap, char *), 1);
+	else if (format == 'p')
+		ft_putaddress_fd(va_arg(ap, unsigned long), 1, &i);
+	else if (format == 'd' || format == 'i')
+		ft_putnbr_fd(va_arg(ap, int), 1, &i);
+	else if (format == 'u')
+		ft_putuibase_fd(va_arg(ap, int), "0123456789", 1, &i);
+	else if (format == 'x')
+		ft_putuibase_fd(va_arg(ap, int), "0123456789abcdef", 1, &i);
+	else if (format == 'X')
+		ft_putuibase_fd(va_arg(ap, int), "0123456789ABCDEF", 1, &i);
+	else if (format == '%')
+		i += ft_putchar_fd('%', 1);
+	return (i);
+}
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	ap;
 	int	f_count;
 	int	i;
+	int	w_count;
 
 	va_start(ap, str);
 	f_count = count_formats(str);
 	if (f_count < 0)
 		return (-1);
-	//printf("count == %d\n", f_count);
 	i = 0;
+	w_count = 0;
 	while (f_count-- + 1)
 	{
 		while (str[i] != '%')
 		{
 			if (str[i] == '\0')
 				return (i);
-			write(1, &str[i], 1);
+			w_count += write(1, &str[i], 1);
 			i++;
 		}
-		//ft_putstr_fd("check\n", 1);
+		w_count += parse_type(str[++i], ap);
 		i++;
-		//write(1, va_arg(ap, char *), 1);
-		//return (-99);
-		ft_putstr_fd(va_arg(ap, char *), 1);
-		i++;
-
-		//put_format(&str[i]);
 	}
-
-	//va_arg(ap, char *);
 	va_end(ap);
-	return (0);
+	return (w_count);
 }
 
+#include <stdio.h>
 //int	main(int argc, char **argv)
 int	main(void)
 {
@@ -104,8 +117,23 @@ int	main(void)
 	//	printf("Give an arg !\n");
 	//	return (0);
 	//}
-	char *str = "[s format]";
-	ft_printf("test %s %s kjashdd\n", str, str);
+	//char *str = "[s format0]";
+	//char *str2 = "[s format1]";
+	//char	c = 'c';
+	//char	*p = str;
+	//char	**dp = &p;
+
+	//ft_printf("ft_printf test %s %s char : %c straddress : %p paddress : %p hex 18 == %p\n", str, str2, c, p, dp, (unsigned long)798754);
+	//printf("printf straddress %p printf paddress %p\n", p, dp);
+	//printf("\n");
+	int	i = 0x153;
+	unsigned int	ui = 4294967295;
+	int	ftcount = 0;
+	ftcount = ft_printf("ft_printf d == %d i == %i ui == %u x == %x X == %X\n", i, i, ui, ui, ui);
+	printf("ftcount == %d\n", ftcount);
+	int	pcount = 0;
+	pcount = printf("cl_printf d == %d i == %i ui == %u x == %x X == %X\n", i, i, ui, ui, ui);
+	printf("pcount == %d\n", pcount);
 	//printf("%s \n", argv[1], argv[2]);
 	//printf("%s %s %s %s %s %s %s \n", argv[1]);
 	//printf("count -> %d\n", count_args(argv[1]));
